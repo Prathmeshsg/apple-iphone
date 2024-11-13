5dd5f3b5f614832c1b21cd43a1a7e5fda135f1d7
Prathmesh Gandhale <125843181+Shadowgit98@users.noreply.github.com>
2024-11-13 15:20:10 +0530

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,15 +9,7 @@ import {
   yellowImg,
 } from "../utils";
 
-export const navLists = [
-  { text: "Store", link: "https://www.apple.com/in/store" },
-  { text: "Mac", link: "https://www.apple.com/in/mac" },
-  { text: "iPhone", link: "https://www.apple.com/in/iphone" },
-  {
-    text: "Support",
-    link: "https://support.apple.com/en-in?cid=gn-ols-home-hp-tab",
-  },
-];
+export const navLists = ["Store", "Mac", "iPhone", "Support"];
 
 export const hightlightsSlides = [
   {
